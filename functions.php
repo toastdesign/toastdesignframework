d581cdd0817552c8de5bc78d140bc480a86922bf
Toast Design <info@toastdesign.nl>
2015-09-18 15:33:55 +0200

    Added google fonts and Font awesome

--- functions.php
+++ functions.php
@@ -116,6 +116,10 @@ add_action( 'widgets_init', 'toast_design_widgets_init' );
 function toast_design_scripts() {
 	wp_enqueue_style( 'toast-design-style', get_stylesheet_uri() );
 
+	wp_enqueue_style('toast-design-fonts', 'https://fonts.googleapis.com/css?family=Oswald:400,300,700|Droid+Sans:400,700');
+
+	wp_enqueue_style('toast-design-icon-fonts', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css');
+
 	wp_enqueue_script( 'toast-design-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
 	wp_enqueue_script( 'toast-design-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
