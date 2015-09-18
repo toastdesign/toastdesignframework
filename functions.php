e4ecdc32e12c584acec9601694d70c642057c323
Toast Design <info@toastdesign.nl>
2015-09-18 16:37:43 +0200

    Added supersfish accesability to the main menu

--- functions.php
+++ functions.php
@@ -122,6 +122,10 @@ function toast_design_scripts() {
 
 	wp_enqueue_script( 'toast-design-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
+	wp_enqueue_script( 'toast-design-superfish', get_template_directory_uri() . '/js/superfish.min.js', array('jquery'), '20150918', true );
+
+	wp_enqueue_script( 'toast-design-superfish-settings', get_template_directory_uri() . '/js/superfish-settings.js', array('toast-design-superfish'), '20150918', true );
+
 	wp_enqueue_script( 'toast-design-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
---
+++ js/superfish-settings.js
@@ -0,0 +1,7 @@
+( function( $ ) {
+	var sf = $('ul.nav-menu');
+	sf.superfish({
+		deflay:200,
+		speed: 'fast'
+	});
+} )( jQuery );
