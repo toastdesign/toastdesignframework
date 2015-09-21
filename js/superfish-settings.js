13c9430829bf4b9bc75fa870fcf276bf3b18411a
Toast Design <info@toastdesign.nl>
2015-09-21 11:56:52 +0200

    Made main navigation responsive

--- js/superfish-settings.js
+++ js/superfish-settings.js
@@ -1,7 +1,23 @@
 ( function( $ ) {
 	var sf = $('ul.nav-menu');
-	sf.superfish({
-		deflay:200,
-		speed: 'fast'
+	var breakpoint = 600;
+
+	if($(document).width() >= breakpoint){
+		sf.superfish({
+			deflay:200,
+			speed: 'fast'
+		});
+	}
+
+	$(window).resize(function(){
+		if($(document).width() >= breakpoint & !sf.hasClass('sf-js-enabled')){
+			sf.superfish({
+			deflay:200,
+			speed: 'fast'
+		});
+	} else if ($(document).width() < breakpoint){
+		sf.superfish('destroy');
 	});
+
+	
 } )( jQuery );
